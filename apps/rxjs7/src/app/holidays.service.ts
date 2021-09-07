413144e20219d1f871c27f6dbe3e5ed3d2f0ea68
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2021-09-07 15:08:23 +0200

    fix typing

--- apps/rxjs7/src/app/holidays.service.ts
+++ apps/rxjs7/src/app/holidays.service.ts
@@ -1,35 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {
-  delay,
-  map,
-  Observable,
-  ReplaySubject,
-  share,
-  Subject,
-  tap,
-  timer,
-} from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { Holiday } from './holiday';
 import { holidayData } from './holiday-data';
 import { HolidaysRequestCounter } from './holidays-request-counter';
 
 @Injectable({ providedIn: 'root' })
 export class HolidaysService {
-  private _holidays$: Observable<Holiday[]>;
-
   constructor(
     private httpClient: HttpClient,
     private holidaysRequestCounter: HolidaysRequestCounter
   ) {
-    const source$ = new Observable((subscriber) => {
+    const source$ = new Observable<Holiday[]>((subscriber) => {
       window.setTimeout(() => subscriber.next(holidayData), 200);
     });
     this._holidays$ = source$.pipe(
-      map(() => holidayData),
       tap(() => this.holidaysRequestCounter.increase())
     );
   }
+
+  private _holidays$: Observable<Holiday[]>;
 
   get holidays$(): Observable<Holiday[]> {
     return this._holidays$;
