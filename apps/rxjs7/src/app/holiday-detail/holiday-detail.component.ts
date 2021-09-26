8cf6261202152ae115b7d0f20722c0dfec101710
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2021-09-26 12:33:01 +0200

    fix subscription in detail

--- apps/rxjs7/src/app/holiday-detail/holiday-detail.component.ts
+++ apps/rxjs7/src/app/holiday-detail/holiday-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnChanges, OnInit } from '@angular/core';
-import { filter, first, map, Observable } from 'rxjs';
+import { filter, first, map, Observable, tap } from 'rxjs';
 import { Holiday } from '../holiday';
 import { HolidaysService } from '../holidays.service';
 
@@ -8,22 +8,31 @@ import { HolidaysService } from '../holidays.service';
   templateUrl: './holiday-detail.component.html',
   styleUrls: ['./holiday-detail.component.scss'],
 })
-export class HolidayDetailComponent implements OnInit {
+export class HolidayDetailComponent {
   @Input() holidayId!: number;
-  holiday$!: Observable<Holiday>;
+  holiday$: Observable<Holiday> = this.holidaysService.holidays$.pipe(
+    map((holidays) =>
+      holidays.find((holiday) => holiday.id === this.holidayId)
+    ),
+    this.filterHoliday
+  );
 
   constructor(private holidaysService: HolidaysService) {}
 
-  ngOnInit() {
-    this.holiday$ = this.holidaysService.holidays$
-      .pipe(
-        map((holidays) =>
-          holidays.find((holiday) => holiday.id === this.holidayId)
-        )
-      )
-      .pipe(
-        filter((holiday) => !!holiday),
-        map((holiday) => holiday as Holiday)
-      );
+  filterHoliday(source$: Observable<Holiday | undefined>) {
+    return source$.pipe(
+      filter((holiday) => !!holiday),
+      map((holiday) => {
+        console.log(holiday);
+        isHoliday(holiday);
+        return holiday;
+      })
+    );
+  }
+}
+
+function isHoliday(holiday: Holiday | undefined): asserts holiday is Holiday {
+  if (holiday === undefined) {
+    throw new Error('no valid holiday');
   }
 }
